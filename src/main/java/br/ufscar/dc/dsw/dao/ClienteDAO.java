03735fd16f0a563a173cce601ee54b9d8ab3a728
boasgsv <boasgsv@protonmail.com>
2023-07-04 07:47:24 -0300

    feat: added a default find function for ClienteDAO and LocadoraDAO
    this default function simply returns null, but allows for the implementation of login logic

--- src/main/java/br/ufscar/dc/dsw/dao/ClienteDAO.java
+++ src/main/java/br/ufscar/dc/dsw/dao/ClienteDAO.java
@@ -1,4 +1,9 @@
 package br.ufscar.dc.dsw.dao;
 
+import br.ufscar.dc.dsw.domain.Cliente;
+
 public class ClienteDAO {
+    public Cliente find(long userId) {
+        return null;
+    }
 }
--- src/main/java/br/ufscar/dc/dsw/dao/LocadoraDAO.java
+++ src/main/java/br/ufscar/dc/dsw/dao/LocadoraDAO.java
@@ -1,4 +1,9 @@
 package br.ufscar.dc.dsw.dao;
 
+import br.ufscar.dc.dsw.domain.Locadora;
+
 public class LocadoraDAO {
+    public Locadora find(long userId) {
+        return null;
+    }
 }
